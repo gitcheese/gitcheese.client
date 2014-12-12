029d1bc5b09a047943e23162f424052ca87ccf0a
mgibas <gibasmaciej@gmail.com>
2014-12-12 17:08:02 +0100

    Added start page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,8 +20,8 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        templateUrl: 'views/start.html',
+        controller: 'StartCtrl'
       })
       .when('/about', {
         templateUrl: 'views/about.html',

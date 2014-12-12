e18f07951d0d4bdb6d741275cbe4db10c21d577b
mgibas <gibasmaciej@gmail.com>
2014-12-13 00:29:56 +0100

    Style refined

--- app/scripts/controllers/start.js
+++ app/scripts/controllers/start.js
@@ -8,10 +8,4 @@
  * Controller of the gitcheeseApp
  */
 angular.module('gitcheeseApp')
-  .controller('StartCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	.controller('StartCtrl', function($scope) {});

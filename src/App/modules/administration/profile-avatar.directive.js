dcb45775e5d00fb2401039f975e2e4a24f3507ea
mgibas <gibasmaciej@gmail.com>
2015-11-22 18:08:02 -0500

    profile avatar

---
+++ src/App/modules/administration/profile-avatar.directive.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('gitcheese.app.administration')
+	.directive('gcProfileAvatar', function () {
+	    var directive = {
+	        restrict: 'E',
+	        templateUrl: 'modules/administration/profile-avatar.directive.html',
+	        replace: true,
+	        scope: {
+	            size: '@',
+	            profileId: '@'
+	        },
+	        controllerAs: 'vm',
+	        bindToController: true,
+	        controller: 'gcProfileAvatarController'
+	    };
+
+	    return directive;
+	});
+
+angular.module('gitcheese.app.administration')
+	.controller('gcProfileAvatarController', function ($scope, apiConstants, $window) {
+	    var vm = this;
+
+	    $scope.$watch('vm.profileId', function () {
+	        if (vm.profileId)
+	            vm.avatarUrl = apiConstants.address[$window.location.hostname] + '/profiles/' + vm.profileId + '/avatars';
+	    });
+	});

9cfaf65b801738c40b85cef693381529881e0c47
mgibas <gibasmaciej@gmail.com>
2016-02-05 19:42:36 -0500

    Pay confirmation page

---
+++ src/App/modules/pledge/pledge-payment.directive.js
@@ -0,0 +1,52 @@
+﻿'use strict';
+
+angular.module('gitcheese.app.pledge')
+	.directive('gcPledgePayment', function () {
+	    return {
+	        replace: true,
+	        templateUrl: 'modules/pledge/pledge-payment.directive.html',
+	        controllerAs: 'vm',
+	        bindToController: true,
+	        controller: 'gcPledgePaymentController',
+	        scope: {
+	            projectId: '@'
+	        }
+	    };
+	});
+
+angular.module('gitcheese.app.pledge')
+    .controller('gcPledgePaymentController', function (Restangular, stripe, notify, $location) {
+        var vm = this;
+        vm.amount = 100;
+        vm.ccTypeToClass = {
+            "American Express": "fa-cc-amex",
+            "Visa": "fa-cc-visa",
+            "MasterCard": "fa-cc-mastercard",
+            "Diners Club": "fa-cc-diners-club",
+            "Discover": "fa-cc-discover",
+            "JCB": "fa-cc-jvb"
+        }
+        vm.initiate = function () {
+            return stripe.card.createToken(angular.copy(vm.card))
+              .then(function (response) {
+                  vm.token = response.id;
+              })
+              .catch(function (err) {
+                  notify({ message: err.message, classes: 'alert alert-danger' });
+              });
+        };
+
+        vm.pledge = function () {
+            return Restangular.one('projects', vm.projectId).post('stripepledges', { token: vm.token, amount: vm.amount })
+                .then(function (pledgeId) {
+                    $location.path('/projects/' + vm.projectId + '/pledges/' + pledgeId + '/confirmed');
+                })
+                .catch(function () {
+                    notify({ message: 'Unknown error :(', classes: 'alert alert-danger' });
+                });
+        }
+
+        vm.change = function () {
+            vm.token = null;
+        }
+    });
--- src/App/modules/pledge/pledge.route.js
+++ src/App/modules/pledge/pledge.route.js
@@ -3,6 +3,5 @@
         $routeProvider
             .when('/projects/:id/details', { templateUrl: 'modules/pledge/projects-details.html' })
             .when('/projects/:projectId/pledges/:id/confirmed', { templateUrl: 'modules/pledge/pledges-confirmed.html', allowAnonymous: true })
-            .when('/projects/:projectId/pledges/:id/canceled', { templateUrl: 'modules/pledge/pledges-canceled.html', allowAnonymous: true })
             .when('/projects/:id/pledges/create', { templateUrl: 'modules/pledge/pledges-create.html', allowAnonymous: true });
     });

58ba308647bf9481213eced405f4f6d164dcad8c
mgibas <gibasmaciej@gmail.com>
2016-02-20 20:54:33 -0500

    Fixed about-us and term-of-use routes

--- src/App/modules/common/common.route.js
+++ src/App/modules/common/common.route.js
@@ -2,6 +2,6 @@
     .config(function ($routeProvider) {
         $routeProvider
             .when('/dashboard', { templateUrl: 'modules/common/dashboard.html' })
-            .when('/about-us', { templateUrl: 'modules/common/about-us.html' })
-            .when('/terms-of-use', { templateUrl: 'modules/common/terms-of-use.html' });
+            .when('/about-us', { templateUrl: 'modules/common/about-us.html', allowAnonymous: true })
+            .when('/terms-of-use', { templateUrl: 'modules/common/terms-of-use.html', allowAnonymous: true });
     });

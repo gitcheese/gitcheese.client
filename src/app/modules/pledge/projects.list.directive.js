98e2152711d641e00f2f75b0279336612e35935a
mgibas <gibasmaciej@gmail.com>
2016-03-07 21:11:30 -0500

    Added refresh button for projects

--- src/app/modules/pledge/projects.list.directive.js
+++ src/app/modules/pledge/projects.list.directive.js
@@ -1,25 +1,35 @@
-﻿'use strict';
+﻿
+'use strict';
 
 angular.module('gitcheese.app.pledge')
-	.directive('gcProjectsList', function () {
-	    return {
-	        replace: true,
-	        templateUrl: 'modules/pledge/projects.list.directive.html',
-	        controllerAs: 'vm',
-	        bindToController: true,
-	        controller: 'gcProjectsListController',
-	        scope: {
-	            caption: '@'
-	        }
-	    };
-	});
+    .directive('gcProjectsList', function() {
+        return {
+            replace: true,
+            templateUrl: 'modules/pledge/projects.list.directive.html',
+            controllerAs: 'vm',
+            bindToController: true,
+            controller: 'gcProjectsListController',
+            scope: {
+                caption: '@'
+            }
+        };
+    });
 
 angular.module('gitcheese.app.pledge')
-    .controller('gcProjectsListController', function (Restangular) {
+    .controller('gcProjectsListController', function(Restangular, notify) {
         var vm = this;
         Restangular.all('projects').getList({
             $orderby: 'Name'
-        }).then(function (projects) {
+        }).then(function(projects) {
             vm.projects = projects;
         });
+
+        vm.refetch = function() {
+            vm.refetchPromise = Restangular.all('projects').customPUT().then(function() {
+                notify({
+                    message: 'Your projects list will be updated pretty soon. Please refresh or come back in a minute.',
+                    classes: 'alert alert-success'
+                });
+            });
+        };
     });

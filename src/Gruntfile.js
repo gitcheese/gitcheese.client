d86da768f209b3278faa23a4d6e306bc6cb53470
mgibas <gibasmaciej@gmail.com>
2016-03-14 20:12:38 -0400

    Cleaning ftp-push status before deploy

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -35,7 +35,8 @@ module.exports = function(grunt) {
                     ]
                 }]
             },
-            dev: '.tmp'
+            dev: '.tmp',
+            deploy: '.grunt/ftpush'
         },
         autoprefixer: {
             options: {
@@ -316,11 +317,12 @@ module.exports = function(grunt) {
     ]);
 
     grunt.registerTask('deploy', [
+        'clean:deploy',
         'ftpush'
     ]);
 
     grunt.registerTask('build-and-deploy', [
         'build',
-        'ftpush'
+        'deploy'
     ]);
 };

7af8ecce5fd0ac7e8aead9254da2136a8d4ecc6d
mgibas <gibasmaciej@gmail.com>
2016-04-28 19:56:48 -0400

    Fixed verification page

--- src/app/modules/administration/verification-editor.directive.js
+++ src/app/modules/administration/verification-editor.directive.js
@@ -1,35 +1,47 @@
-﻿'use strict';
+﻿
+'use strict';
 
 angular.module('gitcheese.app.administration')
-	.directive('gcVerificationEditor', function () {
-	    return {
-	        replace: true,
-	        templateUrl: 'modules/administration/verification-editor.directive.html',
-	        controllerAs: 'vm',
-	        bindToController: true,
-	        controller: 'gcVerificationEditorController',
-	        scope: true
-	    };
-	});
+    .directive('gcVerificationEditor', function() {
+        return {
+            replace: true,
+            templateUrl: 'modules/administration/verification-editor.directive.html',
+            controllerAs: 'vm',
+            bindToController: true,
+            controller: 'gcVerificationEditorController',
+            scope: {
+                profileId: '@'
+            }
+        };
+    });
 
 angular.module('gitcheese.app.administration')
-    .controller('gcVerificationEditorController', function (Restangular, $location, notify, contextService) {
+    .controller('gcVerificationEditorController', function(Restangular, $location, notify) {
         var vm = this;
 
-        vm.verify = function () {
-            var profileId = contextService.profile.id;
-
+        vm.verify = function() {
             Restangular
-                .one('managedaccounts', profileId)
+                .one('managedaccounts', vm.profileId)
                 .one('verifications')
                 .one('1')
-                .post(vm.managedAcct)
-                .then(function () {
-                    notify({ message: 'Verification in process', classes: 'alert alert-success' });
-                    $('#verificationBtn').remove();
-                })
-                .catch(function () {
-                    notify({ message: 'Unknown error :(', classes: 'alert alert-danger' });
+                .customPUT(vm.managedAcct)
+                .then(function() {
+                    vm.verified = true;
+                    notify({
+                        message: 'Verification in process',
+                        classes: 'alert alert-success'
+                    });
+                },function(response){
+									notify({
+											message: response.data.message,
+											classes: 'alert alert-danger'
+									});
+								})
+                .catch(function() {
+									notify({
+											message: 'Unknown error :(',
+											classes: 'alert alert-danger'
+									});
                 });
         };
     });
--- src/app/modules/pledge/verification.required.directive.js
+++ src/app/modules/pledge/verification.required.directive.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 angular.module('gitcheese.app.pledge')
@@ -20,16 +19,16 @@ angular.module('gitcheese.app.pledge')
     .controller('gcVerificationRequiredController', function(Restangular, notify, $scope) {
         var vm = this;
         vm.show = false;
-        //GET /v1/managedaccounts/{id}/verifications/pending
-        $scope.$watch('vm.profileId', function (profileId) {
-            if(profileId == undefined) { return ;}
+        $scope.$watch('vm.profileId', function(profileId) {
+            if (profileId === undefined) {
+                return;
+            }
             Restangular.one('managedaccounts', profileId).one('verifications').one('pending').get()
                 .then(function(pendingVerification) {
-                    if(pendingVerification !== undefined) 
-                    { 
+                    if (pendingVerification !== undefined) {
                         vm.show = true;
-                        vm.url = '/#/profiles/' + profileId + '/verifyaccount'; 
+                        vm.url = '/#/profiles/' + profileId + '/verifyaccount';
                     }
                 });
-            });
+        });
     });

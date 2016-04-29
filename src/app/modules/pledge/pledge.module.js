cf02b568add085e656a9f19bc3c44f2a0e69954e
maciej gibas <gibasmaciej@gmail.com>
2016-04-29 10:45:57 -0400

    Added stripe module as pledge dependency

--- src/app/modules/pledge/pledge.module.js
+++ src/app/modules/pledge/pledge.module.js
@@ -1,3 +1,4 @@
-﻿"use strict";
+﻿
+"use strict";
 
-angular.module('gitcheese.app.pledge', ['angular-stripe', 'credit-cards']);
+angular.module('gitcheese.app.pledge', ['angular-stripe', 'credit-cards', 'gitcheese.app.stripe']);

85161799fd48fb14293d74468e630e9a35e64ffe
mgibas <gibasmaciej@gmail.com>
2016-05-23 20:40:28 -0400

    Added standard badge

--- src/app/modules/pledge/project-badge-markups.directive.js
+++ src/app/modules/pledge/project-badge-markups.directive.js
@@ -1,23 +1,30 @@
-﻿'use strict';
+﻿
+'use strict';
 
 angular.module('gitcheese.app.pledge')
-	.directive('gcProjectBadgeMarkups', function () {
-	    return {
-	        replace: true,
-	        templateUrl: 'modules/pledge/project-badge-markups.directive.html',
-	        controllerAs: 'vm',
-	        bindToController: true,
-	        controller: 'gcProjectBadgeMarkupsController',
-	        scope: {
-	            projectId: '@'
-	        }
-	    };
-	});
+    .directive('gcProjectBadgeMarkups', function() {
+        return {
+            replace: true,
+            templateUrl: 'modules/pledge/project-badge-markups.directive.html',
+            controllerAs: 'vm',
+            bindToController: true,
+            controller: 'gcProjectBadgeMarkupsController',
+            scope: {
+                projectId: '@',
+                type: '@'
+            }
+        };
+    });
 
 angular.module('gitcheese.app.pledge')
-    .controller('gcProjectBadgeMarkupsController', function (apiConstants, appConstants, $window) {
+    .controller('gcProjectBadgeMarkupsController', function(apiConstants, appConstants, $window, $scope) {
         var vm = this;
-
         vm.pledgeUrl = $window.location.origin + appConstants.addressPostfix[$window.location.hostname] + '/#/projects/' + vm.projectId + '/pledges/create';
-        vm.badgeUrl = apiConstants.address[$window.location.hostname] + '/projects/' + vm.projectId + '/badges';
+        $scope.$watch('vm.type', function(type) {
+            if (!type || type === '0') {
+                vm.badgeUrl = apiConstants.address[$window.location.hostname] + '/projects/' + vm.projectId + '/badges';
+            } else {
+                vm.badgeUrl = apiConstants.address[$window.location.hostname] + '/projects/' + vm.projectId + '/badges?type=' + vm.type;
+            }
+        });
     });
--- src/app/modules/pledge/project-badge.directive.js
+++ src/app/modules/pledge/project-badge.directive.js
@@ -1,23 +1,31 @@
-﻿'use strict';
+﻿
+'use strict';
 
 angular.module('gitcheese.app.pledge')
-	.directive('gcProjectBadge', function () {
-	    return {
-	        replace: true,
-	        templateUrl: 'modules/pledge/project-badge.directive.html',
-	        controllerAs: 'vm',
-	        bindToController: true,
-	        controller: 'gcProjectBadgeController',
-	        scope: {
-	            projectId: '@'
-	        }
-	    };
-	});
+    .directive('gcProjectBadge', function() {
+        return {
+            replace: true,
+            templateUrl: 'modules/pledge/project-badge.directive.html',
+            controllerAs: 'vm',
+            bindToController: true,
+            controller: 'gcProjectBadgeController',
+            scope: {
+                projectId: '@',
+                type: '@'
+            }
+        };
+    });
 
 angular.module('gitcheese.app.pledge')
-    .controller('gcProjectBadgeController', function (apiConstants, appConstants, $window) {
+    .controller('gcProjectBadgeController', function(apiConstants, appConstants, $window, $scope) {
         var vm = this;
-
         vm.pledgeUrl = $window.location.origin + appConstants.addressPostfix[$window.location.hostname] + '/#/projects/' + vm.projectId + '/pledges/create';
-        vm.badgeUrl = apiConstants.address[$window.location.hostname] + '/projects/' + vm.projectId + '/badges';
+
+        $scope.$watch('vm.type', function(type) {
+            if (!type || type === '0') {
+                vm.badgeUrl = apiConstants.address[$window.location.hostname] + '/projects/' + vm.projectId + '/badges';
+            } else {
+                vm.badgeUrl = apiConstants.address[$window.location.hostname] + '/projects/' + vm.projectId + '/badges?type=' + vm.type;
+            }
+        });
     });

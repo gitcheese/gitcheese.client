54076bf40df2d29b0723bba294a4f2f0f6005ac4
Maciej Gibas <gibasmaciej@gmail.com>
2017-01-31 21:15:28 -0500

    Added api url to staging

--- src/app/app.constant.js
+++ src/app/app.constant.js
@@ -1,15 +1,16 @@
 ﻿'use strict';
-
 angular.module('gitcheese.app')
     .constant('appConstants', {
-    	addressPostfix: {
-    	    'localhost': '',
-    	    'www.gitcheese.com': '/app'
-    	}
+        addressPostfix: {
+            'localhost': '',
+            'www.gitcheese.com': '/app',
+            'gitcheese-staging.azurewebsites.net': '/app'
+        }
     })
-	.constant('apiConstants', {
-	    address: {
-	        'localhost': 'https://gitcheese-api-staging.apphb.com/v1',
-	        'www.gitcheese.com': 'https://api.gitcheese.com/v1'
-	    }
-	});
+    .constant('apiConstants', {
+        address: {
+            'localhost': 'https://gitcheese-api-staging.apphb.com/v1',
+            'www.gitcheese.com': 'https://api.gitcheese.com/v1',
+            'gitcheese-staging.azurewebsites.net': 'https://gitcheese-api-staging.apphb.com'
+        }
+    });
